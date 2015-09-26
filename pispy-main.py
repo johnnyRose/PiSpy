a7a05c06449261e2534cda78b34cf70776bac941
John Rosewicz <john.rosewicz@gmail.com>
2015-09-25 21:41:10 -0500

    Refactored humiture to be in its own file.

--- pispy-main.py
+++ pispy-main.py
@@ -1,4 +1,4 @@
-import socket, time, serialNumber, subprocess, queue
+import socket, time, serialNumber, subprocess, queue, humiture
 
 server = ('104.208.29.115', 27007)
 
@@ -11,32 +11,19 @@ q = queue.Queue()
 
 humitureCmd = ['sudo','humiture','11','18']
 
-while True:
-	sthp = subprocess.check_output(humitureCmd)
-	sth = str(sthp).split(' ')
-	status = sth[0]
-	while status != "b'OK":
-		print("Failed to read from sensor, trying again.")
-		time.sleep(1)
-		sthp = subprocess.check_output(humitureCmd)
-		sth = str(sthp).split(' ')
-		status = sth[0]
-	sth.append(str(round(time.time())))
-	sth[2] = sth[2][:len(sth[2])-1]
-	sth = sth[1:]
-	q.put(sth)
+while True: 
+	q.put(humiture.getHumiture()) # [temperature, humidity, timeCaptured]
 	while not q.empty():
+                
 		msg = q.get()
-		temperature = msg[0]
-		humidity = msg[1]
-		timeCaptured = msg[2]
-		message = (serial + ";" + temperature+ ";" + humidity + ";" + timeCaptured)
+		message = (serial + ";" + msg[0] +  ";" + msg[1] + ";" + msg[2])
 		print("Attempting to send to server: " + message)
+		
 		try:
 			numSent = clientsocket.send(bytes(message, "UTF-8"))
 			print("Sent " + str(numSent) + " of " + str(len(message)) + " bytes.")
 			if str(numSent) != str(len(message)):
-				print("Message not fully sent, requing message now.")
+				print("Message not fully sent, reqeueing message now.")
 				q.put(msg)
 				break
 				
