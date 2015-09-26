dd116e824b89fb2ad2ec901b5c8f1dacfc9d3cd3
John Rosewicz <john.rosewicz@gmail.com>
2015-09-25 21:01:15 -0500

    Removed exception catching in getSerialNumber. We want to know if anything bad happens trying to get the serial number.

--- serialNumber.py
+++ serialNumber.py
@@ -1,16 +1,11 @@
 # returns the raspberry pi's serial number
-
 def getSerialNumber():
 	serial = "0000000000000000"
 	
-	try:
-		f = open('/proc/cpuinfo', 'r')
-		for line in f:
-			if line[0:6] == 'Serial':
-				serial = line[10:26]
-		f.close()
-	except:
-		serial = "ERROR"
+	f = open('/proc/cpuinfo', 'r')
+	for line in f:
+		if line[0:6] == 'Serial':
+			serial = line[10:26]
+	f.close()
 	
-	return serial	
-
+	return serial
